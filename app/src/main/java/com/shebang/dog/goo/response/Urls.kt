5650a83be87fb5d3f59829157c42b05cf6712f3a
ayumu <e1n18088@oit.ac.jp>
2019-11-22 03:02:41 +0900

    Initial commit

---
+++ app/src/main/java/com/shebang/dog/goo/response/Urls.kt
@@ -0,0 +1,12 @@
+package com.shebang.dog.goo.response
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class Urls {
+
+    @SerializedName("pc")
+    @Expose
+    var pc: String? = null
+
+}
