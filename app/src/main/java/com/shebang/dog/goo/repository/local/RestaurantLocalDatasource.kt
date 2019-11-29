cd3cba92e636674e83cb66189e5100d23f654e96
ShebangDog <e1n18088@oit.ac.jp>
2019-11-29 17:16:46 +0900

    Databaseを使ってDataSourceを実装する

---
+++ app/src/main/java/com/shebang/dog/goo/repository/local/RestaurantLocalDatasource.kt
@@ -0,0 +1,42 @@
+package com.shebang.dog.goo.repository.local
+
+import com.shebang.dog.goo.model.Id
+import com.shebang.dog.goo.model.RestaurantData
+import com.shebang.dog.goo.model.RestaurantStreet
+import com.shebang.dog.goo.repository.RestaurantDataSource
+
+class RestaurantLocalDataSource(private val restaurantDatabase: RestaurantDatabase) :
+    RestaurantDataSource {
+    private val restaurantDao = restaurantDatabase.restaurantDao()
+
+    override suspend fun loadRestaurants(callback: RestaurantDataSource.LoadRestaurantCallback) {
+        restaurantDao.getRestaurantStreet()?.restaurantDataList.also {
+            if (!it.isNullOrEmpty()) callback.onLoad(RestaurantStreet(it))
+            else callback.onFail()
+        }
+    }
+
+    override suspend fun loadRestaurant(
+        id: Id,
+        callback: RestaurantDataSource.GetRestaurantCallback
+    ) {
+        restaurantDao.getRestaurantData(id).also {
+            if (it != null) callback.onGet(it)
+            else callback.onFail()
+        }
+    }
+
+    override suspend fun saveRestaurant(restaurantData: RestaurantData) {
+        restaurantDao.insertRestaurantData(restaurantData)
+    }
+
+    override suspend fun saveRestaurants(restaurantStreet: RestaurantStreet) {
+        restaurantStreet.restaurantDataList.forEach {
+            restaurantDao.insertRestaurantData(it)
+        }
+    }
+
+    override fun deleteRestaurants() {
+        restaurantDao.deleteRestaurantStreet()
+    }
+}
