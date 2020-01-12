0d45fd59cac9a838912f0fcf51f718cd69d40905
ShebangDog <e1n18088@oit.ac.jp>
2020-01-12 18:06:21 +0900

    RecyclerViewのitemをCardViewで囲む

--- app/src/main/java/com/shebang/dog/goo/data/model/Distance.kt
+++ app/src/main/java/com/shebang/dog/goo/data/model/Distance.kt
@@ -2,6 +2,6 @@ package com.shebang.dog.goo.data.model
 
 data class Distance(val value: Double) {
     override fun toString(): String {
-        return "ここから ${value.toInt()} m"
+        return "ここから ${value.toInt()}m"
     }
 }
