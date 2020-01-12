331d884b986d05abfddccdc4276d6f62e5fbc0d6
ShebangDog <e1n18088@oit.ac.jp>
2020-01-12 18:07:44 +0900

    体裁を整える

--- app/src/main/java/com/shebang/dog/goo/ui/CustomApplication.kt
+++ app/src/main/java/com/shebang/dog/goo/ui/CustomApplication.kt
@@ -5,9 +5,7 @@ import com.shebang.dog.goo.di.ApplicationComponent
 import com.shebang.dog.goo.di.DaggerApplicationComponent
 
 open class CustomApplication : Application() {
-    val applicationComponent by lazy {
-        applicationComponentInitializer()
-    }
+    val applicationComponent by lazy { applicationComponentInitializer() }
 
     open fun applicationComponentInitializer(): ApplicationComponent {
         return DaggerApplicationComponent.factory().create(applicationContext)
