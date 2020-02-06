831aaaeb7053a406827cfe0afab2b0a902769609
ShebangDog <e1n18088@oit.ac.jp>
2020-02-07 02:00:50 +0900

    MyDaggerFragmentでLayoutIdを受け取る

---
+++ app/src/main/java/com/shebang/dog/goo/ui/tab/MyDaggerFragment.kt
@@ -0,0 +1,18 @@
+package com.shebang.dog.goo.ui.tab
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import dagger.android.support.DaggerFragment
+
+abstract class MyDaggerFragment(@LayoutRes private val layoutResId: Int) : DaggerFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutResId, container, false)
+    }
+}
--- app/src/main/java/com/shebang/dog/goo/ui/tab/TabbedFragment.kt
+++ app/src/main/java/com/shebang/dog/goo/ui/tab/TabbedFragment.kt
@@ -1,24 +1,13 @@
 package com.shebang.dog.goo.ui.tab
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.DrawableRes
 import androidx.annotation.LayoutRes
-import dagger.android.support.DaggerFragment
 
-abstract class TabbedFragment(@LayoutRes private val layoutResId: Int) : DaggerFragment() {
+abstract class TabbedFragment(@LayoutRes private val layoutResId: Int) :
+    MyDaggerFragment(layoutResId) {
+
     @DrawableRes
     abstract fun getTabIconId(): Int
 
     abstract fun getTabTitle(): String
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(layoutResId, container, false)
-    }
 }
