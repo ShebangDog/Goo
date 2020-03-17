3eb18a09815dd36f8ee60c0a785464be4728655b
ShebangDog <e1n18088@oit.ac.jp>
2020-03-18 03:44:50 +0900

    trivial change

--- app/src/main/java/com/shebang/dog/goo/util/EndlessRecyclerViewScrollListener.kt
+++ app/src/main/java/com/shebang/dog/goo/util/EndlessRecyclerViewScrollListener.kt
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 abstract class EndlessRecyclerViewScrollListener : RecyclerView.OnScrollListener {
     // The minimum amount of items to have below your current scroll position
     // before loading more.
-    private var visibleThreshold = 8
+    private var visibleThreshold = 12
 
     // The current offset index of data you have loaded
     private var currentPage = 0
