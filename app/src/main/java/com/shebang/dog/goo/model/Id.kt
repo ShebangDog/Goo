28a103d2e0c10fa0489b1cc6bf23dac03eba8890
ShebangDog <e1n18088@oit.ac.jp>
2020-04-14 23:15:46 +0900

    Idをvalueで判別する

--- app/src/main/java/com/shebang/dog/goo/model/Id.kt
+++ app/src/main/java/com/shebang/dog/goo/model/Id.kt
@@ -1,3 +1,18 @@
 package com.shebang.dog.goo.model
 
-data class Id(val value: String)
+data class Id(val value: String) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Id
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+}
