5b08fa43ffb04d8afa5306f88b923beeac8c5d92
ShebangDog <e1n18088@oit.ac.jp>
2020-04-14 23:14:56 +0900

    refactoring

--- app/src/main/java/com/shebang/dog/goo/model/Favorite.kt
+++ app/src/main/java/com/shebang/dog/goo/model/Favorite.kt
@@ -1,10 +1,7 @@
 package com.shebang.dog.goo.model
 
-import com.shebang.dog.goo.util.DebugHelper
-
 data class Favorite(val value: Boolean) {
     fun switch(): Favorite {
-        DebugHelper.log("onSwitch")
         return Favorite(!value)
     }
 
