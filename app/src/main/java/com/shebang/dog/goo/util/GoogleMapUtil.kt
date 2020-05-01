70f26f67bbcee59e88fbd02ffc2d6da88f819008
ShebangDog <e1n18088@oit.ac.jp>
2020-05-01 20:19:39 +0900

    GoogleMapUtilを追加する

---
+++ app/src/main/java/com/shebang/dog/goo/util/GoogleMapUtil.kt
@@ -0,0 +1,48 @@
+package com.shebang.dog.goo.util
+
+import com.shebang.dog.goo.model.location.Location
+
+object GoogleMapUtil {
+
+    private const val BaseUrl = "http://maps.google.com/maps"
+
+    fun createUri(userLocation: Location, restaurantLocation: Location): String {
+        val userLocationForQuery =
+            listOf(userLocation.latitude, userLocation.longitude).joinToString(",")
+
+        val restaurantLocationQuery =
+            listOf(restaurantLocation.latitude, restaurantLocation.longitude).joinToString(",")
+
+        val queryItemList = QueryItems.values()
+        val queryValueList = listOf(
+            userLocationForQuery,
+            restaurantLocationQuery,
+            DirFlags.Walk.value
+        )
+
+        val question = "?"
+
+        val queryList = queryItemList.zip(queryValueList).map { pair ->
+            val queryItem = pair.first
+            val queryValue = pair.second
+            val equal = "="
+
+            queryItem.value + equal + queryValue
+        }.joinToString("&")
+
+        return BaseUrl + question + queryList
+    }
+
+
+    private enum class QueryItems(val value: String) {
+        StartAddress("saddr"),
+        DestinationAddress("daddr"),
+        DirectoryFlag("dirflg")
+    }
+
+    private enum class DirFlags(val value: String) {
+        Ride("r"),
+        Drive("d"),
+        Walk("w")
+    }
+}
