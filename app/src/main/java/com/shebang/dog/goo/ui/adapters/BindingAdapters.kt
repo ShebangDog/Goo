d89fd6a489033a68d24a7ca0b6743f6dd9f5b1ea
ShebangDog <e1n18088@oit.ac.jp>
2020-05-04 01:38:31 +0900

    refactor

--- app/src/main/java/com/shebang/dog/goo/ui/adapters/BindingAdapters.kt
+++ app/src/main/java/com/shebang/dog/goo/ui/adapters/BindingAdapters.kt
@@ -6,21 +6,24 @@ import androidx.databinding.BindingAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 @BindingAdapter("android:visibility")
-fun bindVisibility(view: ViewPager2, imageUrlList: List<String>?) {
-    view.visibility = if (imageUrlList.isNullOrEmpty()) View.GONE else View.VISIBLE
+fun bindVisibility(view: ViewPager2, isVisible: Boolean) {
+    view.visibility = booleanToVisibility(isVisible)
 }
 
 @BindingAdapter("android:visibility")
 fun bindVisibility(
     view: com.tbuonomo.viewpagerdotsindicator.DotsIndicator,
-    imageUrlList: List<String>?
+    isVisible: Boolean
 ) {
 
-    val size = imageUrlList?.size ?: 0
-    view.visibility = if (size < 2) View.GONE else View.VISIBLE
+    view.visibility = booleanToVisibility(isVisible)
 }
 
 @BindingAdapter("android:visibility")
 fun bindVisibility(view: TextView, isVisible: Boolean) {
-    view.visibility = if (isVisible) View.VISIBLE else View.GONE
+    view.visibility = booleanToVisibility(isVisible)
+}
+
+private fun booleanToVisibility(isVisible: Boolean): Int {
+    return if (isVisible) View.VISIBLE else View.GONE
 }
